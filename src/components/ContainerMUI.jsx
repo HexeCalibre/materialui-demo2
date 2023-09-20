0e3e2d389efd9aac2307787c05f3ea47a2debea8
IamIronMan2023 <kramsotnas@yahoo.com>
2023-09-20 19:42:35 +0800

    add ContainerMUI

--- src/components/ContainerMUI.jsx
+++ src/components/ContainerMUI.jsx
@@ -7,6 +7,30 @@ const ContainerMUI = () => {
       <Container sx={{ bgcolor: "green" }}>
         <h1>Default Container</h1>
       </Container>
+
+      <Container maxWidth="xs" sx={{ bgcolor: "green" }}>
+        <h1>xs Container</h1>
+      </Container>
+
+      <Container maxWidth="sm" sx={{ bgcolor: "green" }}>
+        <h1>sm Container</h1>
+      </Container>
+
+      <Container maxWidth="md" sx={{ bgcolor: "green" }}>
+        <h1>md Container</h1>
+      </Container>
+
+      <Container maxWidth="lg" sx={{ bgcolor: "green" }}>
+        <h1>lg Container</h1>
+      </Container>
+
+      <Container maxWidth="xl" sx={{ bgcolor: "green" }}>
+        <h1>xl Container</h1>
+      </Container>
+
+      <Container fixed sx={{ bgcolor: "green" }}>
+        <h1>fixed Container</h1>
+      </Container>
     </>
   );
 };
