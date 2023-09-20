55d0045d0d37e5c59e1895551e0bcf672ef5e361
IamIronMan2023 <kramsotnas@yahoo.com>
2023-09-20 19:00:12 +0800

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import ContainerMUI from "./components/ContainerMUI";
+
+function App() {
+  return (
+    <>
+      <ContainerMUI />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/ContainerMUI.jsx
@@ -0,0 +1,14 @@
+import { Container } from "@mui/material";
+import React from "react";
+
+const ContainerMUI = () => {
+  return (
+    <>
+      <Container sx={{ bgcolor: "green" }}>
+        <h1>Default Container</h1>
+      </Container>
+    </>
+  );
+};
+
+export default ContainerMUI;
