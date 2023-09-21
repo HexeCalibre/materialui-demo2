c322b6471f705112254a0ee453d279f1e5fafe7b
IamIronMan2023 <kramsotnas@yahoo.com>
2023-09-21 19:48:26 +0800

    Add Typography

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import ContainerMUI from "./components/ContainerMUI";
 import LayoutMUI from "./components/LayoutMUI";
+import TypographyMUI from "./components/TypographyMUI";
 
 function App() {
   return (
     <>
       <ContainerMUI />
       <LayoutMUI />
+      <TypographyMUI />
     </>
   );
 }
---
+++ src/components/TypographyMUI.jsx
@@ -0,0 +1,20 @@
+import { Typography } from "@mui/material";
+import React from "react";
+
+const TypographyMUI = () => {
+  return (
+    <>
+      <Typography variant="h1">h1 heading</Typography>
+      <Typography variant="h2">h2 heading</Typography>
+      <Typography variant="h3">h3 heading</Typography>
+      <Typography variant="h4">h4 heading</Typography>
+      <Typography variant="h5">h5 heading</Typography>
+      <Typography variant="h6">h6 heading</Typography>
+
+      <Typography variant="subtitle1">Subtitle1</Typography>
+      <Typography variant="subtitle2">Subtitle2</Typography>
+    </>
+  );
+};
+
+export default TypographyMUI;
